3dee129ef21e03e7292d37b5697fb17a0fe5420c
oandrelb <andre.barbosa.1390@gmail.com>
2025-05-14 22:00:51 -0300

    criando o current balance component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import Navbar from "./components/layout/Navbar";
+import CurrentBalance from "./components/layout/CurrentBalance";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-gray-200 to-gray-300">
+    <div className="m-0 p-0 min-h-screen bg-gradient-to-br from-gray-200 to-gray-300 flex flex-col">
       <Navbar />
+      <div className="flex justify-center items-center">
+        <CurrentBalance />
+      </div>
     </div>
   );
 }
---
+++ frontend/src/components/layout/CurrentBalance.jsx
@@ -0,0 +1,10 @@
+function CurrentBalance() {
+  return (
+    <div className="bg-red-800 p-5 mt-[5rem]  w-3/4 flex justify-between">
+      <h2 className="text-3xl font-normal text-gray-800">Current Balance</h2>
+      <span className="text-3xl font-normal text-gray-800">$1000</span>
+    </div>
+  );
+}
+
+export default CurrentBalance;
