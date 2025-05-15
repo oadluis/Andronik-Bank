cbd7667eb9529426290bd25000d6a91fab8462af
oandrelb <andre.barbosa.1390@gmail.com>
2025-05-14 21:28:06 -0300

    mudando o Welcome message

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import Navbar from "./components/layout/Navbar";
 
 function App() {
   return (
-    <div>
+    <div className="min-h-screen bg-gradient-to-br from-gray-200 to-gray-300">
       <Navbar />
     </div>
   );
--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -3,7 +3,7 @@ import Login from "../commom/Login";
 function Navbar() {
   return (
     <nav className="bg-transparent flex justify-between items-center pt-5 pb-5 pl-[4rem] pr-[4rem]">
-      <h1 className="text-2xl text-blue-500 bg-red-500">
+      <h1 className="text-2xl font-medium text-gray-500">
         Welcome to Andronik Bank
       </h1>
 
