ac8b9d11e3fdb9f3a6fa323fbad3daf061d1d86b
oandrelb <andre.barbosa.1390@gmail.com>
2025-05-17 00:31:05 -0300

    estilizando o card de transferencia monetaria

--- frontend/src/components/layout/CurrentBalance.jsx
+++ frontend/src/components/layout/CurrentBalance.jsx
@@ -5,7 +5,7 @@ function CurrentBalance() {
         <h2 className="text-3xl font-medium text-gray-800">Current Balance</h2>
         <p className="text-gray-500 font-medium">As of 23/03/2025</p>
       </div>
-      <span className="text-4xl font-medium text-gray-800">$1000</span>
+      <span className="text-[3rem] font-medium text-gray-800">$1000</span>
     </div>
   );
 }
--- frontend/src/components/layout/TransferMoney.jsx
+++ frontend/src/components/layout/TransferMoney.jsx
@@ -1,19 +1,27 @@
 function TransferMoney() {
   return (
-    <div className="flex flex-col justify-center items-start w-full bg-red-800 px-[3rem] py-[2.7rem] my-4">
-      <span className="text-2xl font-bold">Transfer Money</span>
-      <div className="flex flex-col justify-center items-start w-full h-full bg-blue-800">
-        <div className="flex flex-row justify-start items-start w-full h-full">
-          <input
-            type="text"
-            className="bg-gray-300 opacity-75 rounded-lg mr-[2rem]"
-          />
-          <input type="text" className="bg-gray-300 rounded-lg" />
-        </div>
-        <div>
-          <label>TransferTo</label>
-          <label>amount</label>
-        </div>
+    <div className="flex flex-col justify-center items-start w-full bg-red-800 px-[3rem] py-[1.7rem] my-4 rounded-xl bg-gradient-to-br from-[#ffb003] to-[#ffcb03]">
+      <span className="text-2xl font-bold mb-4">Transfer Money</span>
+      <div className="flex flex-col justify-center items-start w-full h-full">
+        <form className="flex flex-row justify-start items-start w-full h-full gap-10">
+          <div className="flex flex-col justify-center items-center">
+            <input
+              type="text"
+              className="bg-gray-400 opacity-50 rounded-lg p-1 mb-2"
+            />
+            <label>TransferTo</label>
+          </div>
+          <div className="flex flex-col justify-center items-center">
+            <input
+              type="text"
+              className="bg-gray-400 opacity-50 rounded-lg p-1 mb-2"
+            />
+            <label>Amount</label>
+          </div>
+          <button className="bg-gray-400 opacity-50 rounded-lg p-1 mb-2">
+            Transfer
+          </button>
+        </form>
       </div>
     </div>
   );
@@ -26,4 +34,23 @@ export default TransferMoney;
         <label>TransferTo</label>
         <input type="text" />
         <label>Amount</label>
+
+        
+.operation--transfer {
+    background-image: linear-gradient(to top left, #ffb003, #ffcb03);
+  }
+*/
+
+/*
+<div className="flex flex-row justify-start items-start w-full h-full">
+          <input
+            type="text"
+            className="bg-gray-500 opacity-50 rounded-lg mr-[2rem] p-2"
+          />
+          <input type="text" className="bg-gray-300 rounded-lg p-2" />
+        </div>
+        <div>
+          <label>TransferTo</label>
+          <label>amount</label>
+        </div>
 */
