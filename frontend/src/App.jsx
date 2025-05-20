e90e4485bbdc0baef99cdbc5c87d7e83f7680485
oandrelb <andre.barbosa.1390@gmail.com>
2025-05-19 21:23:23 -0300

    criando o footer e arrumando a altura do app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,15 +4,16 @@ import CurrentLogs from "./components/layout/Stratum";
 import TransferMoney from "./components/layout/TransferMoney";
 import RequestLoan from "./components/layout/RequestLoan";
 import CloseAccount from "./components/layout/CloseAccount";
+import Footer from "./components/layout/Footer";
 
 function App() {
   return (
-    <div className="m-0 p-0 min-h-screen bg-gradient-to-br from-gray-100 to-gray-200 flex flex-col">
+    <div className="h-[100dvh] bg-gradient-to-br from-gray-100 to-gray-200 flex flex-col">
       <Navbar />
-      <div className="flex flex-col justify-center items-center">
+      <div className="h-full flex flex-col justify-center items-center">
         <CurrentBalance />
         <div className="flex justify-center items-center w-3/4">
-          <div className="flex flex-col justify-center items-start w-2/4 pl-5 pr-5">
+          <div className="flex flex-col justify-center items-start w-2/4 px-5">
             <CurrentLogs />
           </div>
           <div className="flex flex-col justify-around items-start w-2/4">
@@ -20,6 +21,9 @@ function App() {
             <RequestLoan />
             <CloseAccount />
           </div>
+        </div>
+        <div>
+          <Footer />
         </div>
       </div>
     </div>
--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -3,7 +3,7 @@ import imageLogo from "../../assets/logo.png";
 
 function Navbar() {
   return (
-    <nav className="bg-transparent flex justify-between items-center pt-5 pb-5 pl-[4rem] pr-[4rem]">
+    <nav className="bg-transparent flex justify-between items-center pt-5 pb-0 pl-[4rem] pr-[4rem]">
       <h1 className="text-2xl font-medium text-gray-500">
         Welcome to Andronik Bank
       </h1>
