48c75121e48c52233da42f9526d754690dfa1101
andronikSeven <andre.barbosa.1390@gmail.com>
2025-05-26 08:17:34 -0300

    adicionando o backend da aplicação

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -1,13 +1,16 @@
+import {ArrowDownIcon} from "@heroicons/react/24/outline"
+
 function Footer() {
   return (
     <footer className="w-full flex px-[10rem] py-5 justify-between">
       <div className="flex flex-row">
-        <p>TESTE</p>
-        <p>TESTE</p>
-        <p>TESTE</p>
+        <p>in</p>
+        <p>5080$</p>
+        <p>out</p>
+        <p>1000$</p>
       </div>
 
-      <button>buttonTeste</button>
+      <button className="flex flex-row align-center"><ArrowDownIcon className="size-6" /> SORT</button>
 
       <span>You Will be logout in 5:00 minutes</span>
     </footer>
