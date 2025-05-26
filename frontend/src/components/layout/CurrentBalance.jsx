d34a16a440996e0a85f5e63478ca4f9a16a676b7
andronikSeven <andre.barbosa.1390@gmail.com>
2025-05-26 15:52:19 -0300

    adicionando as datas dinamicamente

--- frontend/src/components/layout/CurrentBalance.jsx
+++ frontend/src/components/layout/CurrentBalance.jsx
@@ -1,9 +1,11 @@
 function CurrentBalance({currentUser}) {
+  const currentDate = new Date();
+
   return (
     <div className=" p-5  w-3/4 flex justify-between">
       <div>
         <h2 className="text-3xl font-medium text-gray-800">Current Balance</h2>
-        <p className="text-gray-500 font-medium">As of 23/03/2025</p>
+        <p className="text-gray-500 font-medium">{`As of ${currentDate.getDate()}/${currentDate.getMonth()}/${currentDate.getFullYear()}`}</p>
       </div>
       <p></p>
       <span className="text-[3rem] font-medium text-gray-800">{`$${currentUser?.movements.reduce((acumulador, valorAtual) => {return acumulador + valorAtual}, 0)}`}</span>
