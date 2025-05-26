db519bb2531ae07ac1a579954d4883f4dd5a7115
andronikSeven <andre.barbosa.1390@gmail.com>
2025-05-26 15:43:56 -0300

    adicionando o valor que tem na conta dinamicamente, o valor é a soma de todos os movimentos juntos.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,11 +11,6 @@ import Footer from "./components/layout/Footer";
 function App() {
   const [currentUser, setCurrentUser] = useState(null)
 
-  // Função async que faz a requisição dos dados para a API
-  // Fazer a chamada para a api soliciando os dados
-  // Tratar o erro de requisição (login falhou)
-  // Manter esses dados e enviar para o resto da aplicação
-
   const handleLogin = async (username, pin) => {
     try{
       const response = await fetch('http://localhost:3000/api/v1/login', {
@@ -42,14 +37,13 @@ function App() {
     }
   }
 
-
   return (
     <div className="h-[100dvh] bg-gradient-to-br from-gray-100 to-gray-200 flex flex-col">
       <Navbar onLoginSubmit={handleLogin} currentUser={currentUser}/>
 
       {currentUser ? (
         <div className="h-full flex flex-col justify-center items-center">
-        <CurrentBalance />
+        <CurrentBalance currentUser={currentUser} />
         <div className="flex justify-center items-center w-3/4">
           <div className="flex flex-col justify-center items-start w-2/4 px-5">
             <CurrentLogs />
--- frontend/src/components/layout/CurrentBalance.jsx
+++ frontend/src/components/layout/CurrentBalance.jsx
@@ -1,11 +1,13 @@
-function CurrentBalance() {
+function CurrentBalance({currentUser}) {
+
   return (
     <div className=" p-5  w-3/4 flex justify-between">
       <div>
         <h2 className="text-3xl font-medium text-gray-800">Current Balance</h2>
         <p className="text-gray-500 font-medium">As of 23/03/2025</p>
       </div>
-      <span className="text-[3rem] font-medium text-gray-800">$1000</span>
+      <p></p>
+      <span className="text-[3rem] font-medium text-gray-800">{`$${currentUser?.movements.reduce((acumulador, valorAtual) => {return acumulador + valorAtual}, 0)}`}</span>
     </div>
   );
 }
