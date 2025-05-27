3025cb6509393c040153484ce05be5b6604de4b1
andronikSeven <andre.barbosa.1390@gmail.com>
2025-05-27 18:18:16 -0300

    mudando a classe dos cards dinamicamente de acordo com o tipo da movimentação

--- frontend/src/components/commom/StratumCard.jsx
+++ frontend/src/components/commom/StratumCard.jsx
@@ -1,7 +1,13 @@
 function StratumCard({ movementValue }) {
   return (
     <div className="flex justify-between items-center w-full h-auto p-5">
-      <span className="font-medium text-white bg-gradient-to-tl from-[#39b385] to-[#9be15d] px-4 py-2 rounded-full">
+      <span
+        className={`font-medium text-white ${
+          movementValue > 0
+            ? `bg-gradient-to-tl from-[#39b385] to-[#9be15d]`
+            : `bg-red-800`
+        }  px-4 py-2 rounded-full`}
+      >
         1 deposit
       </span>
       <span className="text-gray-500 font-medium">${movementValue}</span>
