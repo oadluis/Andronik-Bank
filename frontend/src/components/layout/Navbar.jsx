6aaeacf91deb97fb5823ffbd5b5e30bf05e413c6
andronikSeven <andre.barbosa.1390@gmail.com>
2025-05-30 15:14:43 -0300

    arrumando as proporções do header

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -1,25 +1,31 @@
-import Login from "../commom/Login";
+import Login from '../commom/Login';
 import { BanknotesIcon } from '@heroicons/react/24/outline';
 
-function Navbar({onLoginSubmit, currentUser}) {
+function Navbar({ onLoginSubmit, currentUser }) {
   return (
-    <nav className="bg-white flex justify-between align-center py-4 px-[1rem] shadow-md">
+    <nav className="sticky top-0 z-10 flex flex-col md:flex-row justify-between items-center p-4 bg-white shadow-md">
       {currentUser ? (
-        <div className="flex items-center gap-4">
-        <div>
-          <BanknotesIcon className="size-10 p-1 text-white bg-purple-500 rounded-full"/>
-        </div>
-        <div className={`flex flex-col align-center`}>
-          <h1 className="text-xl font-bold">Andronik Bank</h1>
-          <br/>
-          <div className={`flex gap-2`}>
-          <p className="text-gray-500 font-medium">Bem vindo,</p>
-          <p className={`text-purple-800 font-medium`}>{` ${currentUser.owner.split(' ')[0]}!`}</p>
+        <div className="flex items-center mb-4 md:mb-0">
+          <div>
+            <BanknotesIcon className="size-8 p-1 text-white bg-purple-500 rounded-full mr-3" />
+          </div>
+          <div className={`flex flex-col align-center`}>
+            <h1 className="text-xl font-bold">Andronik Bank</h1>
+            <br />
+            <div className={`flex gap-2`}>
+              <p className="text-sm text-gray-500">
+                Bem vindo,{' '}
+                <span className="text-purple-800 font-medium">{`${
+                  currentUser.owner.split(' ')[0]
+                }!`}</span>
+              </p>
+            </div>
           </div>
         </div>
-        </div>
-      ) : <h1 className="text-2xl font-bold">Andronik Bank</h1>}
-      <Login onLoginSubmit={onLoginSubmit} currentUser={currentUser}/>
+      ) : (
+        <h1 className="text-2xl font-bold">Andronik Bank</h1>
+      )}
+      <Login onLoginSubmit={onLoginSubmit} currentUser={currentUser} />
     </nav>
   );
 }
