9374a8e316919510e764f450ca58a826936e4348
andronikSeven <andre.barbosa.1390@gmail.com>
2025-06-02 23:45:38 -0300

    corrigindo o bug da UI do componente RequestLoan

--- frontend/src/components/layout/operations/RequestLoan.jsx
+++ frontend/src/components/layout/operations/RequestLoan.jsx
@@ -1,23 +1,30 @@
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import { BanknotesIcon } from '@heroicons/react/24/solid';
 
 function RequestLoan() {
   return (
     <form className="bg-white p-5 rounded-xl shadow-md">
       <div className="flex justify-between items-center mb-4">
         <h3 className="font-semibold text-lg">Solicitar empréstimo</h3>
-      </div>
-      <div className="w-10 h-10 bg-green-100 rounded-full flex items-center justify-center">
-        <label className="block text-sm font-medium text-gray-700 mb-1">
-          Valor do empréstimo
-        </label>
-        <div className="relative">
-          <span className="absolute left-3 top-3 text-gray-700">R$</span>
-          <input
-            type="text"
-            className="pl-8 w-full px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-300 focus:border-blue-500"
-            placeholder="0,00"
-          />
+        <div className="w-10 h-10 bg-green-100 rounded-full flex items-center justify-center">
+          <BanknotesIcon className="size-6 text-green-600" />
         </div>
+      </div>
+      <div className="space-y-3">
+        <div>
+          <label className="block text-sm font-medium text-gray-700 mb-1">
+            Valor do empréstimo
+          </label>
+          <div className="relative">
+            <span className="absolute left-3 top-3 text-gray-700">R$</span>
+            <input
+              type="text"
+              className="pl-8 w-full px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-300 focus:border-blue-500"
+              placeholder="0,00"
+            />
+          </div>
+        </div>
+
         <div>
           <label className="block text-sm font-medium text-gray-700 mb-1">
             Parcelas
